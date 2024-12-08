319f87c9501287bc873297f3c6233595e8302ebc
Gvozdem <gvozdenko.demid@mail.ru>
2024-12-08 12:21:28 +0200

    Update objects

--- Assets/Scripts/CheckpointManager.cs
+++ Assets/Scripts/CheckpointManager.cs
@@ -4,22 +4,36 @@ using UnityEngine.SceneManagement;
 
 public class CheckpointManager : MonoBehaviour
 {
-    public Transform[] checkpointTransforms;
+    public Transform[] cheackpointTransforms;
     private vThirdPersonController controller;
     private int currentCheckpointIndex = 0;
 
-    void Start()
+    private float lastInputTime = 0f;
+    private float idleTimeLimit = 60f; // 60 секунд
+
+    private Transform platformsParent;
+
+    private void Start()
     {
+        lastInputTime = Time.time;
         controller = GetComponent<vThirdPersonController>();
-        if (checkpointTransforms.Length == 0)
+        if (cheackpointTransforms.Length == 0)
         {
             // Установите стартовую точку как чекпоинт, если они не заданы
-            checkpointTransforms = new[] { transform };
+            cheackpointTransforms = new[] { transform };
+
+            // Найти родителя "Platforms"
+            platformsParent = GameObject.Find("Platforms").transform;
+
+            // Инициализировать массив checkpointTransforms
+            InitializeCheckpointTransforms();
         }
+
     }
 
-    void Update()
+    private void Update()
     {
+        CheckPlayerIdle();
         if (controller.isGrounded || (!controller.isGrounded && Input.GetMouseButtonUp(0)))
         {
             controller.airSpeed = 5;
@@ -35,24 +49,51 @@ public class CheckpointManager : MonoBehaviour
             SetCheckpoint(currentCheckpointIndex);
 
             // Если достигнут последний чекпоинт, переходим на следующую сцену
-            if (currentCheckpointIndex == checkpointTransforms.Length - 1)
+            if (currentCheckpointIndex == cheackpointTransforms.Length - 1)
             {
                 LoadNextScene();
             }
         }
 
         // Возрождаем персонажа на чекпоинте, если он упал
-        if (!controller.isGrounded && transform.position.y < checkpointTransforms[currentCheckpointIndex].position.y - 5f)
+        if (!controller.isGrounded && transform.position.y < cheackpointTransforms[currentCheckpointIndex].position.y - 5f)
         {
             Respawn();
         }
     }
 
-    bool IsOnCheckpoint()
+    private void InitializeCheckpointTransforms()
     {
-        for (int i = 0; i < checkpointTransforms.Length; i++)
+        // Получить все дочерние объекты платформ
+        Transform[] platformTransforms = platformsParent.GetComponentsInChildren<Transform>();
+
+        // Найти все объекты чекпоинтов и добавить их в массив
+        int checkpointCount = 0;
+        foreach (Transform child in platformTransforms)
         {
-            if (Vector3.Distance(transform.position, checkpointTransforms[i].position) < 5f && currentCheckpointIndex < i)
+            if (child.Find("Cheackpoint") != null)
+            {
+                checkpointCount++;
+            }
+        }
+        Debug.Log(checkpointCount);
+        cheackpointTransforms = new Transform[checkpointCount];
+        checkpointCount = 0;
+        foreach (Transform child in platformTransforms)
+        {
+            if (child.Find("Cheackpoint") != null)
+            {
+                Debug.Log($"{child.Find("Cheackpoint").transform}");
+                cheackpointTransforms[checkpointCount++] = child.Find("Cheackpoint").transform;
+            }
+        }
+    }
+
+    private bool IsOnCheckpoint()
+    {
+        for (int i = 0; i < cheackpointTransforms.Length; i++)
+        {
+            if (Vector3.Distance(transform.position, cheackpointTransforms[i].position) < 5f && currentCheckpointIndex < i)
             {
                 currentCheckpointIndex = i;
 
@@ -62,24 +103,39 @@ public class CheckpointManager : MonoBehaviour
         return false;
     }
 
-    void SetCheckpoint(int index)
+    private void CheckPlayerIdle()
+    {
+        // Обновляем время последнего действия игрока
+        if (controller.input.x != 0 || controller.input.z != 0 || Input.GetButton("Jump"))
+        {
+            lastInputTime = Time.time;
+        }
+
+        // Если игрок бездействует больше 60 секунд, переходим на "MenuScene"
+        if (Time.time - lastInputTime >= idleTimeLimit)
+        {
+            SceneManager.LoadScene("MenuScene");
+        }
+    }
+
+    private void SetCheckpoint(int index)
     {
         Debug.Log($"New checkpoint set at index {index}!");
     }
 
-    void Respawn()
+    private void Respawn()
     {
-        transform.position = checkpointTransforms[currentCheckpointIndex].position + new Vector3(0,1,0);
-        transform.rotation = checkpointTransforms[currentCheckpointIndex].rotation;
+        transform.position = cheackpointTransforms[currentCheckpointIndex].position + new Vector3(0,1,0);
+        transform.rotation = cheackpointTransforms[currentCheckpointIndex].rotation;
         Debug.Log($"Respawned at checkpoint {currentCheckpointIndex}!");
     }
 
-    void LoadNextScene()
+    private void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        if (currentSceneIndex == 10)
+        if (currentSceneIndex == 9)
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(0);
         }
         else
         {
